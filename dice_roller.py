9ef59b63098ad1a9f5cbb5341257068ec74ae6de
Renan Monteiro Chaves Leite <renan.leite@petrobras.com.br>
2020-12-12 01:02:31 -0300

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,12 @@
 def main():
-  print('You rolled a die')
+    from random import randint
+    dice_rolls = 2
+    dice_sum = 0
+    for _ in range(dice_rolls):
+        roll = randint(1,6)
+        dice_sum += roll
+        print(f'You rolled a {roll}')
+    print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
-  main()
+    main()
